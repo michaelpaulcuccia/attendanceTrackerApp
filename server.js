65e9bf8b86c262054f7b1cc6dbee49c0ec7fffa6
Michael-Paul Cuccia <michaelpaulcuccia@yahoo.com>
2020-10-15 17:14:26 -0400

    require(path) was missing quotations

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const path = require(path);
+const path = require('path');
 require('dotenv').config();
 
 const app = express();
